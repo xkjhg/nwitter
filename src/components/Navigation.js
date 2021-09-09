105ab80b58918c121df7e2378b7a34234c6d21e3
xkjhg <xkjhg@naver.com>
2021-09-09 22:43:27 +0900

    10. CSS - Navigation 1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "components/Navigation.css"
+import { getDownloadURL, ref } from "firebase/storage"
+import { storage } from "fBase"
 
 const Navigation = ({ userObj }) => {
+  const logoUrl = getDownloadURL(ref(storage, "nwitter/logo.jpg"))
   return (
-    <nav>
+    <nav className="nav">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img src={logoUrl} width="50px" height="50px" />
+          </Link>
         </li>
         <li>
           <Link to="/profile">
