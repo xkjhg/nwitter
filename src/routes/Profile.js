59c6aadbc3b8669dfc83f6f5925181b4ad7ad933
xkjhg <xkjhg@naver.com>
2021-09-19 22:20:23 +0900

    12. Profile Image

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,6 @@
-import { auth } from "fBase"
+import { auth, storage } from "fBase"
 import { updateProfile } from "firebase/auth"
+import { getDownloadURL, ref, uploadString } from "firebase/storage"
 import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
 import "routes/Profile.css"
@@ -7,6 +8,7 @@ import "routes/Profile.css"
 const Profile = ({ refreshUser, userObj }) => {
   const History = useHistory()
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
+  const [profileImg, setProfileImg] = useState("")
   const onLogOutClick = () => {
     auth.signOut()
     History.push("/")
@@ -15,12 +17,34 @@ const Profile = ({ refreshUser, userObj }) => {
     const { target: { value } } = event
     setNewDisplayName(value)
   }
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault()
+    let profileImgUrl = ""
+    if (profileImg !== "") {
+      const attachmentRef = ref(storage, `${userObj.uid}/profileImg`)
+      const response = await uploadString(attachmentRef, profileImg, "data_url")
+      profileImgUrl = await getDownloadURL(response.ref)
+    }
     if (userObj.displayName !== newDisplayName) {
       updateProfile(auth.currentUser, {
         displayName: newDisplayName
       }).then(() => refreshUser())
+    } else if (userObj.photoURL !== profileImgUrl) {
+      updateProfile(auth.currentUser, {
+        photoURL: profileImgUrl
+      })
+    }
+  }
+  const onFileChange = event => {
+    const { target: { files } } = event
+    const theFile = files[0]
+    const reader = new FileReader()
+    reader.onloadend = finishedEvent => {
+      const { target: { result } } = finishedEvent
+      setProfileImg(result)
+    }
+    if (theFile) {
+      reader.readAsDataURL(theFile)
     }
   }
   return (
@@ -32,6 +56,7 @@ const Profile = ({ refreshUser, userObj }) => {
           value={newDisplayName}
           onChange={onChange}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="UPDATE" />
       </form>
       <button onClick={onLogOutClick}>LOG OUT</button>
